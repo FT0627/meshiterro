01a5dcbcf85292969fb9e1ad58d53c5727c7fc7e
FT0627 <fairytail1926@gmail.com>
2024-09-11 14:38:34 +0000

    made edit update in post_image and added to code for a draft function in the  post_images_controller

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -6,8 +6,19 @@ class Public::PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
+    
+    if params[:draft].present?
+      @post_image.status = :draft
+    else
+      @post_image.status = :published
+    end 
+    
     if @post_image.save
-      redirect_to post_images_path
+      if @post_image.draft?
+        redirect_to post_images_path
+      else
+        redirect_to post_images_path
+      end
     else
       render :new
     end
@@ -27,6 +38,40 @@ class Public::PostImagesController < ApplicationController
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
+  end
+  
+  def edit
+    @post_image = PostImage.find(params[:id])
+    if @post_image.user_id == current_user.id
+      render :edit
+    else
+      flash.now[:alert] = "この投稿は編集できません。"
+      render :show
+    end
+  end
+  
+  def update
+    @post_image = PostImage.find(params[:id])
+    if params[:draft].present?
+      @post_image.status = :draft
+      flash[:notice] = "下書きを保存しました。"
+      redirect_to post_images_path
+    elsif params[:unpublished].present?
+      @post.status = :unpublished
+      flash[:notice] = "非公開にしました。"
+      redirect_to post_images_path
+    else
+      @post_image.status = :publish
+      flash[:notice] = "投稿を更新しました。"
+      redirect_to post_image_path(@post_image)
+    end
+    
+    if @post_image.update(post_image_params)
+      redirect_to post_image_path(@post_image)
+    else
+      render :edit
+    end 
+    
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     devise_for :users
     root to: "homes#top"
     get "homes/about", to: "homes#about", as: :about
-    resources :post_images, only: [:new, :create, :index, :show, :destroy]  do
+    resources :post_images, only: [:new, :create, :index, :show, :destroy, :edit, :update]  do
       resources :post_comments, only: [:create, :destroy]
       resource :favorite, only: [:create, :destroy]
     end
